408be544f5a3bff0f76ef9e5687a3f3b519b4e70
Tien Vu <thomastien123@gmail.com>
2024-10-05 11:19:25 -0600

    Replace deprecated method and remove unnecessary

--- As1_Testcase.py
+++ As1_Testcase.py
@@ -21,60 +21,60 @@ class Assignment1Test(unittest.TestCase):
 
         try:
             child.expect(wexpect.EOF, timeout=0.1)
-            return re.findall(".+" ,child.before.strip())[0]
+            return child.before.strip()
         except wexpect.TIMEOUT:
             return "pass"
 
     def test_citizen(self):
-        self.assertEquals(self.get_out_from_in(["Nah"]), "Invalid response.")
-        self.assertEquals(self.get_out_from_in(["Ye"]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(["Nah"]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(["Ye"]), "Invalid response.")
         for ans in self.valid_ans:
-            self.assertEquals(self.get_out_from_in([ans]), "pass")
+            self.assertEqual(self.get_out_from_in([ans]), "pass")
 
     def test_albertan(self):
-        self.assertEquals(self.get_out_from_in(["Yes", "Nah"]), "Invalid response.")
-        self.assertEquals(self.get_out_from_in(["Yes","Ye"]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Nah"]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(["Yes","Ye"]), "Invalid response.")
         for ans in self.valid_ans:
-            self.assertEquals(self.get_out_from_in(["Yes", ans]), "pass")
+            self.assertEqual(self.get_out_from_in(["Yes", ans]), "pass")
 
     def test_month(self):
         pass_ans = ["Yes", "Yes"]
-        self.assertEquals(self.get_out_from_in(pass_ans + ["january"]), "Invalid response.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["Augst"]), "Invalid response.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["May"]), "pass")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["December"]), "pass")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["january"]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["Augst"]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["May"]), "pass")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["December"]), "pass")
 
     def test_year(self):
         pass_ans = ["Yes", "Yes", "January", 10]
-        self.assertEquals(self.get_out_from_in(pass_ans + [1890]), "Invalid response.")
-        self.assertEquals(self.get_out_from_in(pass_ans + [2026]), "Invalid response.")
-        self.assertEquals(self.get_out_from_in(pass_ans + [1900]), "You are eligible to vote.")
-        self.assertEquals(self.get_out_from_in(pass_ans + [2024]), "You are not eligible to vote.")
-        self.assertEquals(self.get_out_from_in(pass_ans + [2000]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + [1890]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(pass_ans + [2026]), "Invalid response.")
+        self.assertEqual(self.get_out_from_in(pass_ans + [1900]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + [2024]), "You are not eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + [2000]), "You are eligible to vote.")
 
     # month, day, year
     def test_valid_bday(self):
         pass_ans = ["Yes", "Yes"]
-        self.assertEquals(self.get_out_from_in(pass_ans + ["January", 32,2000]), "Invalid birth date.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["February", 29, 2011]), "Invalid birth date.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["October", 15, 2024]), "Invalid birth date.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["September", 28, 2024]), "Invalid birth date.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["August", 15, 2024]), "You are not eligible to vote.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["September", 10, 2024]), "You are not eligible to vote.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["August", 31, 2000]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["January", 32,2000]), "Invalid birth date.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["February", 29, 2011]), "Invalid birth date.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["October", 15, 2024]), "Invalid birth date.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["September", 28, 2024]), "Invalid birth date.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["August", 15, 2024]), "You are not eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["September", 10, 2024]), "You are not eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["August", 31, 2000]), "You are eligible to vote.")
 
         # Test leap year
-        self.assertEquals(self.get_out_from_in(pass_ans + ["February", 29, 2000]), "You are eligible to vote.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["February", 29, 1900]), "Invalid birth date.")
-        self.assertEquals(self.get_out_from_in(pass_ans + ["February", 29, 2004]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["February", 29, 2000]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["February", 29, 1900]), "Invalid birth date.")
+        self.assertEqual(self.get_out_from_in(pass_ans + ["February", 29, 2004]), "You are eligible to vote.")
 
     def test_eligibility(self):
-        self.assertEquals(self.get_out_from_in(["Yes", "Yes", "January", 20, 2000]), "You are eligible to vote.")
-        self.assertEquals(self.get_out_from_in(["Yes", "Yes", "January", 20, 2006]), "You are eligible to vote.")
-        self.assertEquals(self.get_out_from_in(["Yes", "Yes", "September", 25, 2006]), "You are eligible to vote.")
-        self.assertEquals(self.get_out_from_in(["Yes", "Yes", "September", 28, 2006]), "You are not eligible to vote.")
-        self.assertEquals(self.get_out_from_in(["Yes", "Yes", "October", 2, 2006]), "You are not eligible to vote.")
-        self.assertEquals(self.get_out_from_in(["Yes", "Yes", "January", 20, 2007]), "You are not eligible to vote.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Yes", "January", 20, 2000]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Yes", "January", 20, 2006]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Yes", "September", 25, 2006]), "You are eligible to vote.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Yes", "September", 28, 2006]), "You are not eligible to vote.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Yes", "October", 2, 2006]), "You are not eligible to vote.")
+        self.assertEqual(self.get_out_from_in(["Yes", "Yes", "January", 20, 2007]), "You are not eligible to vote.")
 
 
 if __name__ == "__main__":
